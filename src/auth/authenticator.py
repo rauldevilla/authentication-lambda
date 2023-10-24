688294b787ed84da0e378f6124059b88e8f316fb
rauldevilla <raul.devilla@gmail.com>
2023-10-23 21:14:48 -0500

    Implements authentication using dao

--- src/auth/authenticator.py
+++ src/auth/authenticator.py
@@ -1,3 +1,6 @@
+import uuid
+from dao.users_dao import UsersDAO
+from model.user import User
 
 class Authenticator:
 
@@ -13,12 +16,21 @@ class Authenticator:
         
         return True
 
+    def __compare_credentials__(user:User, credentials:dict) -> bool:
+        return  user.login == credentials["login"] and \
+                user.password == credentials["password"]
+
     def authenticate(self, credentials:dict) -> str:
 
         if not self.__valid_credentials__(credentials):
             return None
 
-        if credentials["login"] == "raul.devilla" and credentials["password"] == "abc":
-            return "123abc456"
+        dao_users:UsersDAO = UsersDAO()
+        user:User = dao_users.get(credentials.login)
+        if not user:
+            return None
+        
+        if self.__compare_credentials__(user, credentials):
+            return uuid.uuid4()
         
         return None
